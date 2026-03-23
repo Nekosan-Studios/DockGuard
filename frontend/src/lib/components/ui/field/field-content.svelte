31d7fe4049e966cde1da9ca8b26f6d18d06c27f3
Matt Weinecke <748754+MattTW@users.noreply.github.com>
2026-03-23 15:30:04 -0700

    update shadcn-svelte to latest

--- frontend/src/lib/components/ui/field/field-content.svelte
+++ frontend/src/lib/components/ui/field/field-content.svelte
@@ -14,7 +14,7 @@
   bind:this={ref}
   data-slot="field-content"
   class={cn(
-    "group/field-content flex flex-1 flex-col gap-1.5 leading-snug",
+    "gap-0.5 group/field-content flex flex-1 flex-col leading-snug",
     className
   )}
   {...restProps}
--- frontend/src/lib/components/ui/spinner/spinner.svelte
+++ frontend/src/lib/components/ui/spinner/spinner.svelte
@@ -3,13 +3,22 @@
   import Loader2Icon from "@lucide/svelte/icons/loader-2";
   import type { ComponentProps } from "svelte";
 
-  let { class: className, ...restProps }: ComponentProps<typeof Loader2Icon> =
-    $props();
+  let {
+    class: className,
+    role = "status",
+    // we add color and stroke for compatibility with different icon libraries props
+    color,
+    stroke,
+    "aria-label": ariaLabel = "Loading",
+    ...restProps
+  }: ComponentProps<typeof Loader2Icon> = $props();
 </script>
 
 <Loader2Icon
-  role="status"
-  aria-label="Loading"
+  {role}
+  color={color === null ? undefined : color}
+  stroke={stroke === null ? undefined : stroke}
+  aria-label={ariaLabel}
   class={cn("size-4 animate-spin", className)}
   {...restProps}
 />
